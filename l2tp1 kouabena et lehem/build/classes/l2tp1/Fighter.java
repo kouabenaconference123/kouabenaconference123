83bacab7ad4f1a175d5607d271adc814ae07a007
unknown <aristidekouabe@gmail.com>
2024-02-09 10:52:32 +0100

    TP ROBOT

---
+++ l2tp1 kouabena et lehem/build/classes/l2tp1/Fighter.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package l2tp1;
+
+
+ import java.util.Random;
+
+/**
+ *
+ * @author SIME FIDELE
+ */
+public // Définition de la classe Fighter qui hérite de la classe Robot
+class Fighter extends Robot {
+   
+
+    // Constructeur de la classe Fighter
+    public Fighter(String name, long seed) {
+        super(name); // Appel au constructeur de la classe parent (Robot)
+        System.out.println("Fighter " + name);
+    }
+
+    // Méthode pour attaquer un autre robot avec une chance aléatoire de toucher
+    @Override
+    public void fire(Robot target) {
+        if (!this.isDead() && !target.isDead()) {
+                target.health -= 2;
+                System.out.println("Fighter " + this.name + " a touché le Robot " + target.name + " !");
+            } else {
+                System.out.println("Fighter " + this.name + " a raté sa cible !");
+            }
+        }
+    }
+//}
